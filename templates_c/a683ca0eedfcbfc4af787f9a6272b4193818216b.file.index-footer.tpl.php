b47d1f4878037e89fb59cc6054b0efc338081a9a
= <japtapp@gmail.com>
2013-01-24 14:50:50 +0800

    * first commit for jan242013.

---
+++ templates_c/a683ca0eedfcbfc4af787f9a6272b4193818216b.file.index-footer.tpl.php
@@ -0,0 +1,23 @@
+<?php /* Smarty version Smarty 3.1.4, created on 2012-02-10 12:50:39
+         compiled from "templates\index-footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:229394f35129f87da12-41133286%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'a683ca0eedfcbfc4af787f9a6272b4193818216b' => 
+    array (
+      0 => 'templates\\index-footer.tpl',
+      1 => 1328102614,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '229394f35129f87da12-41133286',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty 3.1.4',
+  'unifunc' => 'content_4f35129f8877a',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4f35129f8877a')) {function content_4f35129f8877a($_smarty_tpl) {?></body>
+</html><?php }} ?>
